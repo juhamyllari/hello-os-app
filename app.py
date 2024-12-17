29518b2e460427bda58039d7b4b38cfde5d2bec2
Juha Myllari <jmyllari@gmail.com>
2024-12-17 21:02:12 +0200

    Integrate db

--- app.py
+++ app.py
@@ -11,12 +11,16 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-conn = psycopg2.connect(
-    host="postgresql",
-    database="mydb",
-    user="user",
-    password="password"
-)
+try:
+    conn = psycopg2.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
+    )
+    print("Database connection successful!")
+except Exception as e:
+    print(f"Database connection failed: {e}")
 
 app = Flask(__name__)
 
