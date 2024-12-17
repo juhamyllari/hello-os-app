3d16cb352d87bfaa9246f80deafe11e9ebc80279
Juha Myllari <jmyllari@gmail.com>
2024-12-17 20:45:20 +0200

    Prepare for db addition

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
 from flask import Flask
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+conn = psycopg2.connect(
+    host="postgresql",
+    database="mydb",
+    user="user",
+    password="password"
+)
+
 app = Flask(__name__)
 
 @app.route('/')
